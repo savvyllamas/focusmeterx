54c7a2287ce6f5151c20aafcb93ef313e565de32
Dmitry Sologub <dd.sologub@gmail.com>
2019-12-10 15:56:19 +0700

    Fix app name in titles

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,7 @@
 <template>
   <div id="app">
     <div id="nav">
+      <h2>FocusMeterX</h2>
       <router-link to="/">Home</router-link> |
       <router-link to="/about">About</router-link>
     </div>
