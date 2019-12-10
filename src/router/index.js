00d4bb233e024398956d5cd330da7315bf9d9f3f
Dmitry Sologub <dd.sologub@gmail.com>
2019-12-10 15:54:57 +0700

    Add pages for authorization

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,36 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Home from '../views/Home.vue'
+import Login from '../views/Login.vue'
+import SignUp from '../views/SignUp.vue'
+import NotFound from '../views/NotFound.vue'
+import About from '../views/About.vue'
+import firebase from 'firebase'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
+    redirect: '/login'
+  },
+  {
+    path: '/login',
+    name: 'Login',
+    component: Login
+  },
+  {
+    path: '/signup',
+    name: 'SignUp',
+    component: SignUp
+  },
+  {
+    path: '/home',
     name: 'home',
-    component: Home
+    component: Home,
+    meta: {
+      requiresAuth: true
+    }
   },
   {
     path: '/about',
@@ -16,7 +38,13 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    // component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: About
+  },
+  {
+    path: '*',
+    name: 'NotFound',
+    component: NotFound
   }
 ]
 
@@ -24,6 +52,15 @@ const router = new VueRouter({
   mode: 'history',
   base: process.env.BASE_URL,
   routes
+})
+
+router.beforeEach((to, from, next) => {
+  let currentUser = firebase.auth().currentUser
+  let requiresAuth = to.matched.some(record => record.meta.requiresAuth)
+
+  if (requiresAuth && !currentUser) next('login')
+  else if (!requiresAuth && currentUser) next('home')
+  else next()
 })
 
 export default router
---
+++ src/views/Login.vue
@@ -0,0 +1,46 @@
+<template>
+  <div class="login">
+    <img src="../assets/logo.png" width="192px">
+    <h3>FocusMeterX</h3>
+    <input
+      type="text"
+      v-model="email"
+      placeholder="Email address"
+      class="input"
+      required>
+    <br/>
+    <input
+      type="password"
+      v-model="password"
+      placeholder="Password"
+      class="input"
+      required>
+    <br/>
+    <button v-on:click="login" class="button">Enter</button>
+    <p><router-link to="/signup">
+      New Here? Create a new account
+    </router-link></p>
+  </div>
+</template>
+
+<script>
+// import firebase from 'firebase'
+export default {
+  name: 'login',
+  data: function () {
+    return {
+      email: '',
+      password: ''
+    }
+  },
+  methods: {
+    login () {
+      // firebase.auth().signInWithEmailAndPassword(this.email, this.password).then((user) => {
+      //   this.$router.replace('/tests')
+      // }).catch((err) => {
+      //   alert(err.message)
+      // })
+    }
+  }
+}
+</script>
